98e49abc5f68ab7250c7222cbfc105ac1c38e772
qianlv7 <qianlv7@qq.com>
2022-05-09 10:49:57 +0800

    add wslg config

--- zsh/user_config.zsh
+++ zsh/user_config.zsh
@@ -113,8 +113,10 @@ case "$OSTYPE" in
 
             # X11 configure
             # https://stackoverflow.com/questions/61110603/how-to-set-up-working-x11-forwarding-on-wsl2
-            export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
-            export LIBGL_ALWAYS_INDIRECT=1
+            # export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+            # wslg
+            export DISPLAY=:0
+            # export LIBGL_ALWAYS_INDIRECT=1
         else
             alias pbcopy='xsel --clipboard --input'
             alias pbpaste='xsel --clipboard --output'
